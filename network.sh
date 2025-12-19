894d23cd6c07103fa71dfc0e4e49386007103902
ph <phil.hartel26@gmail.com>
2025-12-19 21:47:04 +0100

    Chnage ip

--- network.sh
+++ network.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-ip address add 192.168.100.6/24 broadcast + dev ${NET_IFACE}
+ip address add 192.168.100.7/24 broadcast + dev ${NET_IFACE}
 ip route add default via 192.168.100.1 dev ${NET_IFACE}
 timedatectl
