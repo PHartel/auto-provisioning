a854a3cafbcdba716fd603177ffde45743a18bec
ph <phil.hartel26@gmail.com>
2025-12-27 10:48:09 +0100

    Fix config

--- mirror.sh
+++ mirror.sh
@@ -2,3 +2,4 @@
 
 pacman -Sy --noconfirm reflector
 reflector --country Germany --latest 10 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+echo "Updated mirrorlist with the fastest German mirrors."
