5fd96adab9acffc4f7a660345245908ec4fd2e4e
ph <phil.hartel26@gmail.com>
2025-12-27 15:14:03 +0100

    Fix

--- user-management.sh
+++ user-management.sh
@@ -6,7 +6,9 @@ useradd -m -G wheel $USERNAME
 echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
 passwd
 passwd $USERNAME
-cp -r /root/opt/setup /root/home/$USERNAME/
-chown -R $USERNAME:$USERNAME /root/home/$USERNAME/.local
 
+# Copy without that the path is /root/home/username/.local and not /root/home/username/setup/.local
+cp -r /root/opt/setup/.local /root/home/$USERNAME/
+chown -R $USERNAME:$USERNAME /root/home/$USERNAME/.local
+cp -r /root/opt/setup/.config /root/home/$USERNAME/
 chown -R $USERNAME:$USERNAME /root/home/$USERNAME/.config
