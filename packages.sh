30ba3202981bc0b03998bc473667cdfe46f77369
ph <phil.hartel26@gmail.com>
2025-12-27 15:00:44 +0100

    Fix path

--- packages.sh
+++ packages.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-pacman -S --needed --noconfirm $(cat packages.txt)
+pacman -S --needed --noconfirm $(cat /root/opt/setup/packages.txt)
 pacman -R --noconfirm plasma-welcome drkonqi plasma-systemmonitor flatpak-kcm flatpak
--- user-management.sh
+++ user-management.sh
@@ -4,8 +4,8 @@ useradd -m -G wheel $USERNAME
 echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
 passwd
 passwd $USERNAME
-cp -r .local /root/home/$USERNAME/
+cp -r /root/opt/setup/.local /root/home/$USERNAME/
 chown -R $USERNAME:$USERNAME /root/home/$USERNAME/.local
 
-cp -r .config /root/home/$USERNAME/
+cp -r /root/opt/setup/.config /root/home/$USERNAME/
 chown -R $USERNAME:$USERNAME /root/home/$USERNAME/.config
