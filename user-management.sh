95ddab3f4ea0d8aa83ba7ae975a58da92752db71
ph <phil.hartel26@gmail.com>
2025-12-27 14:57:43 +0100

    Fix

--- user-management.sh
+++ user-management.sh
@@ -4,8 +4,8 @@ useradd -m -G wheel $USERNAME
 echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
 passwd
 passwd $USERNAME
-cp -r .local /mnt/root/home/$USERNAME/
-chown -R $USERNAME:$USERNAME /mnt/root/home/$USERNAME/.local
+cp -r .local /root/home/$USERNAME/
+chown -R $USERNAME:$USERNAME /root/home/$USERNAME/.local
 
-cp -r .config /mnt/root/home/$USERNAME/
-chown -R $USERNAME:$USERNAME /mnt/root/home/$USERNAME/.config
+cp -r .config /root/home/$USERNAME/
+chown -R $USERNAME:$USERNAME /root/home/$USERNAME/.config
